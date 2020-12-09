8ad22b833811f94d978e1daa07935e8df07fe31d
alifmaulanarizqi <alifmaulana312@gmail.com>
2020-12-09 20:07:23 +0700

    menambahkan dummy data pada database

--- laundry.sql
+++ laundry.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 09, 2020 at 09:45 AM
+-- Generation Time: Dec 09, 2020 at 02:06 PM
 -- Server version: 10.4.16-MariaDB
 -- PHP Version: 7.4.12
 
@@ -39,7 +39,7 @@ CREATE TABLE `customers` (
 --
 
 INSERT INTO `customers` (`id`, `nama`, `jml_transaksi`, `email`) VALUES
-(50, 'test1', 3, 'test1@email.com');
+(50, 'test1', 6, 'test1@email.com');
 
 -- --------------------------------------------------------
 
@@ -109,11 +109,11 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `nama`, `harga`, `keterangan`, `jml_transaksi`) VALUES
-('LNDRY1', 'Produk Laundry 1', '10000', 'Cuci + Setrika', 3),
+('LNDRY1', 'Produk Laundry 1', '10000', 'Cuci + Setrika', 5),
 ('LNDRY2', 'Produk Laundry 2', '7000', 'Cuci', 0),
 ('LNDRY3', 'Produk Laundry 3', '6000', 'Setrika', 0),
 ('LNDRY4', 'Produk Laundry 4', '13000', 'Cuci + Setrika + Cepat', 0),
-('LNDRY5', 'Produk Laundry 5', '15000', 'Cuci + Setrika + Cepat + Wangi', 0);
+('LNDRY5', 'Produk Laundry 5', '15000', 'Cuci + Setrika + Cepat + Wangi', 1);
 
 -- --------------------------------------------------------
 
@@ -134,9 +134,13 @@ CREATE TABLE `total_transactions` (
 
 INSERT INTO `total_transactions` (`tanggal`, `total`, `bulan`, `total_per_bulan`) VALUES
 ('2020-11-01', '20000', 'November', '20000'),
-('2020-12-09', '10000', 'December', '10000'),
-('2020-12-10', '0', 'December', '10000'),
-('2021-02-01', '10000', 'February', '10000');
+('2020-12-09', '10000', 'December', '40000'),
+('2020-12-10', '0', 'December', '40000'),
+('2020-12-11', '30000', 'December', '40000'),
+('2021-01-01', '50000', 'January', '50000'),
+('2021-02-01', '10000', 'February', '10000'),
+('2021-03-01', '45000', 'March', '45000'),
+('2021-04-01', '0', 'April', '0');
 
 -- --------------------------------------------------------
 
@@ -160,7 +164,10 @@ CREATE TABLE `transactions` (
 INSERT INTO `transactions` (`id`, `id_produk`, `id_customer`, `berat_laundry`, `tanggal`, `harga`) VALUES
 (117, 'LNDRY1', 50, 1, '2020-12-09', '10000'),
 (118, 'LNDRY1', 50, 1, '2021-02-01', '10000'),
-(129, 'LNDRY1', 50, 2, '2020-11-01', '20000');
+(129, 'LNDRY1', 50, 2, '2020-11-01', '20000'),
+(130, 'LNDRY1', 50, 3, '2020-12-11', '30000'),
+(131, 'LNDRY1', 50, 5, '2021-01-01', '50000'),
+(132, 'LNDRY5', 50, 3, '2021-03-01', '45000');
 
 --
 -- Indexes for dumped tables
@@ -228,7 +235,7 @@ ALTER TABLE `positions`
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=130;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=134;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
