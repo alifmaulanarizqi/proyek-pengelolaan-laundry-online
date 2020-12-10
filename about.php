cb6d508df30e93321b7268c7434a3af31cff483d
Tezar-Irawan <73024145+Tezar-Irawan@users.noreply.github.com>
2020-12-11 00:41:12 +0800

    Halaman about

---
+++ about.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About</title>
+	<style type="text/css">
+		.header1{
+			margin-left: 30px ;
+		}
+
+		.header2{
+			background-color: black;
+			color: white;
+			padding: 10px;
+			text-align: center;
+		}
+
+		.badankiri{
+			float: left;
+			padding: 5px;
+			margin-right: 50px;
+			padding-right: 200px;
+			margin-top: 50px;
+			border-right: 5px solid black;
+		}
+
+		.badankanan{
+			margin-top: 75px;
+		}
+
+		a{
+			text-decoration: none;
+			color: black;
+		}
+
+		a:hover{
+			color: grey;
+		}
+
+
+		footer{
+			background-color: black;
+			margin-top: 30px;
+			padding: 10px;
+		}
+
+		img{
+			width: 300px;
+		}
+
+		footer{
+			background-color: black;
+			margin-top: 30px;
+			padding: 10px;
+		}
+
+		.footer td{
+			padding: 10px;
+		}
+
+		.tim td{
+			padding: 10px;
+			text-align: center;
+			font-size: 30px;
+		}
+
+	</style>
+</head>
+<body>
+
+	<div class="header1">
+		<h2><a href="index.php">Laundry Dong</a> | About</h2>
+	</div>
+
+	<div class="header2">
+		<h1>The Great Team!</h1>
+	</div>
+
+	<div class="badankiri">
+			<p><a href="" style="color: red">TEAM</a></p>
+			<p><a href="visi_about.php">VISI</a></p>
+			<p><a href="misi_about.php">MISI</a></p>
+			<p><a href="latarbelakang.php">LATAR BELAKANG</a></p>
+	</div>
+
+	<div class="badankanan">	
+		<h1>TEAM LAUNDRY DONG</h1>
+		<table class="tim">
+			<tr>
+				<td><img src="images/alif.png"></td>
+				<td><img src="images/tezar.png"></td>
+				<td><img src="images/adminn.png"></td>
+			</tr>
+			<tr>
+				<td>Alif Maulana Risqi</td>
+				<td>Tezar Irawan</td>
+				<td>Hadin Alfitrah H</td>
+			</tr>
+		</table>
+	</div>
+
+	<footer>
+		<table class="footer">
+			<tr>
+				<td>
+					<a href="support.php" style="color: white">Support </a>
+				</td>
+				<td>
+					<a href="" style="color: white">Contact</a>
+				</td>
+				<td style="text-align: right; color: white; width: 100%" >
+					Copyright &copy; 2020 <a href="#" class="link-style-none" style="color: white">Kelompok5</a>. All Rights Reserved
+				</td>
+			</tr>
+		</table>
+	</footer>
+
+</body>
+</html>
---
+++ misi_about.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About</title>
+	<style type="text/css">
+		.header1{
+			margin-left: 30px ;
+		}
+
+		.header2{
+			background-color: black;
+			color: white;
+			padding: 10px;
+			text-align: center;
+		}
+
+		.badankiri{
+			float: left;
+			padding: 5px;
+			margin-right: 50px;
+			padding-right: 200px;
+			margin-top: 50px;
+			border-right: 5px solid black;
+		}
+
+		.badankanan{
+			margin-top: 75px;
+			width: 800px;
+			margin-left: 400px;
+		}
+
+		a{
+			text-decoration: none;
+			color: black;
+		}
+
+		a:hover{
+			color: grey;
+		}
+
+
+		footer{
+			background-color: black;
+			margin-top: 30px;
+			padding: 10px;
+		}
+
+		img{
+			width: 200px;
+		}
+
+		footer{
+			background-color: black;
+			margin-top: 30px;
+			padding: 10px;
+		}
+
+		.footer td{
+			padding: 10px;
+		}
+
+		.tim td{
+			padding: 10px;
+			text-align: center;
+			font-size: 30px;
+		}
+
+	</style>
+</head>
+<body>
+
+	<div class="header1">
+		<h2><a href="index.php">Laundry Dong</a> | About</h2>
+	</div>
+
+	<div class="header2">
+		<h1>Misi Kami!</h1>
+	</div>
+
+	<div class="badankiri">
+			<p><a href="about.php">TEAM</a></p>
+			<p><a href="visi_about.php">VISI</a></p>
+			<p><a href="" style="color: red">MISI</a></p>
+			<p><a href="latarbelakang.php">LATAR BELAKANG</a></p>
+	</div>
+
+	<div class="badankanan">
+		<h1><b>MISI LAUNDRY DONG</b></h1>
+		<img src="images/Laundrydong.png" >
+		<p style="text-indent: 50px; font-size: 20px; text-align: justify;">
+			Misi kami adalah terus mengembangkan <b>Landry dong</b> dan menyesuaikan dengan semua kebutuhan user. Fitur-fitur yang ada didalamnya pun akan terus bertambah dengan adanya kebutuhan-kebutuhan yang diinginkan user yang menggunakan <b>Laundry dong</b>.Kami juga akan membantu user dalam penggunakan <b>Laundry dong</b> dengan benar agar penggunaan lebih efesien.
+
+		</p>
+	</div>
+
+	<footer>
+		<table class="footer">
+			<tr>
+				<td>
+					<a href="support.php" style="color: white">Support </a>
+				</td>
+				<td>
+					<a href="" style="color: white">Contact</a>
+				</td>
+				<td style="text-align: right; color: white; width: 100%" >
+					Copyright &copy; 2020 <a href="#" class="link-style-none" style="color: white">Kelompok5</a>. All Rights Reserved
+				</td>
+			</tr>
+		</table>
+	</footer>
+
+</body>
+</html>
